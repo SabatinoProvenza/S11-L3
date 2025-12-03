932a4ec31eefc52b1db81d41982eb4982d87f11c
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2025-12-03 14:14:05 +0100

    creates redux base

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+  prefs: {
+    content: [],
+  },
+}
+
+const mainReducer = function (currentState = initialState, action) {
+  switch (action.type) {
+    // case
+
+    default:
+      return currentState
+  }
+}
+
+export default mainReducer
